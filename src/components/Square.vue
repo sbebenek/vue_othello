78d0281c645b167417da6bf442d95007e2ee89ed
sbebenek <36522668+sbebenek@users.noreply.github.com>
2020-07-01 19:03:32 -0400

    Add game piece sounds and animations

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="square" v-on:click="setPiece($event)">
-    <div v-if="gamePiece === 'white'">
-      <div class="whitePiece"></div>
+    <div v-if="this.isAnimated === true">
+      <div v-if="gamePiece === 'white'">
+        <div class="whitePiece animated" :style="cssVars"></div>
+      </div>
+      <div v-else-if="gamePiece === 'black'">
+        <div class="blackPiece animated" :style="cssVars"></div>
+      </div>
     </div>
-    <div v-else-if="gamePiece === 'black'">
-      <div class="blackPiece"></div>
+    <div v-else>
+      <div v-if="gamePiece === 'white'">
+        <div class="whitePiece"></div>
+      </div>
+      <div v-else-if="gamePiece === 'black'">
+        <div class="blackPiece"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 module.exports = {
-  props: ["piece", "row", "column"], //can be white, black, or null
+  props: ["piece", "row", "column", "isAnimated", "animationDelay", "sound"], //can be white, black, or null
+  //animation delay will delay the time the animation is set to play, allowing each piece to be flipped incrementally
   //watch for prop changes
   watch: {
     piece: function(newVal, oldVal) {
@@ -28,11 +39,15 @@ module.exports = {
     return {
       gamePiece: this.piece,
       squareRow: this.row,
-      squareColumn: this.column
+      squareColumn: this.column,
+      //isAnimated: this.isAnimated,
+      //animationDelay: this.animationDelay //will be a whole number ie 1,2,3,4, etc.
     };
   },
   methods: {
     setPiece: function(event) {
+      console.log("animation delay " + this.animationDelay)
+
       this.$parent.setPiece(
         event,
         this.$props.piece,
@@ -40,7 +55,19 @@ module.exports = {
         this.$props.column
       );
     }
+  },
+
+
+  
+
+  computed: {
+    cssVars() {
+      return {
+        'animation-delay': this.animationDelay * 0.05 + 's' //convert the whole number to a small, incremental ms value
+      }
+    }
   }
+
 };
 </script>
 
@@ -64,13 +91,28 @@ module.exports = {
   border-radius: 50%;
   background: white;
   border: 1px solid whitesmoke;
-
 }
 .blackPiece {
   width: 3vw;
   height: 3vw;
   border-radius: 50%;
   background: black;
-  border: 1px solid rgb(100,100,100);
+  border: 1px solid rgb(100, 100, 100);
+}
+
+.animated {
+  opacity: 0.0;
+  animation: dropPiece 0.1s ease-out 1 forwards;
+}
+
+@keyframes dropPiece {
+  from {
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0px);
+  }
 }
 </style>
---
+++ src/sound.js
@@ -0,0 +1,18 @@
+/**
+ * Loads a sound file into the page
+ * source - https://www.w3schools.com/graphics/game_sound.asp
+ */
+export function Sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
+}
